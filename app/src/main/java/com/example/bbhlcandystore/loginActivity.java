3f9d55eeea85f8d6a679754914f56bfba5e679c9
hoanglong123000 <hoanglong123000@gmail.com>
2021-10-08 22:57:06 +0700

    -Finished Login page UI. -In processing of writting DBHelper.java source code.

---
+++ app/src/main/java/com/example/bbhlcandystore/loginActivity.java
@@ -0,0 +1,17 @@
+package com.example.bbhlcandystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class loginActivity extends AppCompatActivity {
+    private TextView reglink;
+    private Button sibtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
