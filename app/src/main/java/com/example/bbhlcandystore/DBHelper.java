193ecae33c2c378ab3b6765c0e253bc515d02148
hoanglong123000 <hoanglong123000@gmail.com>
2021-10-09 22:09:40 +0700

    -Finished Login page UI. -In processing of writting DBHelper.java source code. -Finished Register page UI.

--- app/src/main/java/com/example/bbhlcandystore/DBHelper.java
+++ app/src/main/java/com/example/bbhlcandystore/DBHelper.java
@@ -2,35 +2,41 @@ package com.example.bbhlcandystore;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper{
+    // Initialize a variable DBName = name of database in String.
     private static final String DBName= "login.db";
 
-    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    // Constructor of DBHelper.
+    public DBHelper(Context context) {
         super(context, "login.db", null, 1);
     }
 
+    // This function is overwritten(Override) to create a data table in order to create database.
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-        sqLiteDatabase.execSQL("create Table logindatabase(username TEXT primary key, pass TEXT)");
+        String createdatabase = "create Table logindatabase(username TEXT primary key, pass TEXT)";
+        sqLiteDatabase.execSQL(createdatabase);
     }
 
+    // This function will delete data table in database if there is available database.
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("drop Table if exists logindatabase");
     }
 
+    // Function to insert information into database.
     public Boolean insertData(String username, String pass)
     {
         SQLiteDatabase mydb = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("username", username);
-        contentValues.put("password", pass);
+        contentValues.put("pass", pass);
         long res = mydb.insert("logindatabase", null, contentValues);
         if (res == -1)
         {
@@ -39,5 +45,34 @@ public class DBHelper extends SQLiteOpenHelper{
         else
             return true;
     }
+
+    // Function to check username in information of database whether it exists or not.
+    public Boolean checkusername(String username)
+    {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("Select * from logindatabase where username = ?", new String[] { username});
+        if (cursor.getCount() > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Function to check username and its password of database.
+    public Boolean usernamepassword(String username, String password)
+    {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("Select * from logindatabase where username = ? and pass = ?", new String[]{username, password});
+        if (cursor.getCount() > 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
 
 }
--- app/src/main/java/com/example/bbhlcandystore/loginActivity.java
+++ app/src/main/java/com/example/bbhlcandystore/loginActivity.java
@@ -2,16 +2,43 @@ package com.example.bbhlcandystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity {
+    private EditText USERNAME;
+    private EditText PASSWORD;
     private TextView reglink;
     private Button sibtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        USERNAME = (EditText) findViewById(R.id.loginusernametxt);
+        PASSWORD = (EditText) findViewById(R.id.loginpasswordtxt);
+        sibtn = (Button) findViewById(R.id.signinbtn);
+        reglink = (TextView) findViewById(R.id.registerlink);
+
+        // Function of sign up here link.
+        reglink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openregpage();
+            }
+        });
     }
+
+    // Function of sign up here link.
+    public void openregpage()
+    {
+        Intent openregisterpage = new Intent(this, MainActivity.class);
+        startActivity(openregisterpage);
+    }
+
 }
