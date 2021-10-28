61a4694b30828ce55aec662b1d1892040d4ff03c
Yukack <hoanglong123000@gmail.com>
2021-10-28 22:13:20 +0700

    -Finished coding source code: StartupActivity.java -Finished coding Back-end of Startup page.

--- app/src/main/java/com/example/bbhlcandystore/StartupActivity.java
+++ app/src/main/java/com/example/bbhlcandystore/StartupActivity.java
@@ -2,13 +2,70 @@ package com.example.bbhlcandystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class StartupActivity extends AppCompatActivity {
+
+    private TextView signuplnk;
+    private Button signinbtn;
+    private RadioGroup switchsides;
+    private RadioButton adminside;
+    private RadioButton clientside;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+        signuplnk = (TextView) findViewById(R.id.signuplink1);
+        signinbtn = (Button) findViewById(R.id.signinbysidesbtn);
+        adminside = (RadioButton) findViewById(R.id.adminchckbox1);
+        clientside = (RadioButton) findViewById(R.id.clientchckbox1);
+
+        // Sign up link.
+        signuplnk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signuplinkf();
+            }
+        });
+
+        // Sign in between sides button.
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signinbtwnsidesbtn();
+            }
+        });
     }
+
+
+    // Link to sign up in pages.
+    public void signuplinkf()
+    {
+        Intent switchtosignuppage = new Intent(this, registeractivity.class);
+        startActivity(switchtosignuppage);
+    }
+
+    // Sign in button.
+    public void signinbtwnsidesbtn()
+    {
+        // Cases to switch between login pages.
+        if(adminside.isChecked())
+        {
+            Intent switchtosigninadminpage = new Intent(this, loginActivity.class);
+            startActivity(switchtosigninadminpage);
+        }
+        else
+        {
+            Intent switchtosigninclientpage = new Intent(this, ClientLoginActivity.class);
+            startActivity(switchtosigninclientpage);
+        }
+    }
+
 }
--- app/src/main/java/com/example/bbhlcandystore/registeractivity.java
+++ app/src/main/java/com/example/bbhlcandystore/registeractivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
